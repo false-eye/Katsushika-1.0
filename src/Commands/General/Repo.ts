c2a0e597779816016c56744d100731de36bfb3ce
一茶 <Issamoha020@gmail.com>
2023-02-17 18:12:03 +0300

    Update Repo.ts

--- src/Commands/General/Repo.ts
+++ src/Commands/General/Repo.ts
@@ -15,8 +15,7 @@ export default class extends BaseCommand {
         }
         const image = this.client.assets.get('img') as Buffer
         const text = `*━━━❰ Bot Repo ❱━━━*\n\n⚜𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: An anime themed WhatsApp bot written in Typescript using baileys.\n\n🚥𝗦𝘁𝗮𝘁𝘂𝘀: Bot in development stage.\n\n🧩𝗛𝗶𝗻𝘁: The bot is an open source project, therefore you can deploy a version of it;
-(https://github.com/Issa2001/Katsushika)\n\n⭐𝗜𝗻𝗳𝗼: This bot is using a base of WhatsApp-bot, we therefore don't have any copyright or either affiliated with WhatsApp-bot anyhowly. Thanks to Lucky Yambem for base bot.
-(https://github.com/LuckyYam/WhatsApp-bot)\n\n📃𝗟𝗶𝗰𝗲𝗻𝘀𝗲: You may obtain a copy of the License at;
+(https://github.com/Issa2001/Katsushika)\n\n⭐𝗜𝗻𝗳𝗼: This bot is public & free to use.\n\n📃𝗟𝗶𝗰𝗲𝗻𝘀𝗲: You may obtain a copy of the License at;
 http://www.gnu.org/licenses/\n\n(𝗚𝗡𝗨 𝗔𝗙𝗙𝗘𝗥𝗢 𝗚𝗘𝗡𝗘𝗥𝗔𝗟 𝗣𝗨𝗕𝗟𝗜𝗖 𝗟𝗜𝗖𝗘𝗡𝗦𝗘).
 »𝖵𝖾𝗋𝗌𝗂𝗈𝗇 3.0 \n`
         return void (await reply(image, 'image', undefined, undefined, text, undefined, {
