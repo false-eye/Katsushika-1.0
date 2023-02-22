1aff713dd7d629c039358654981c941c3831624e
一茶 <Issamoha020@gmail.com>
2023-02-22 12:29:12 +0300

    Update Ban.ts

--- src/Commands/Dev/Ban.ts
+++ src/Commands/Dev/Ban.ts
@@ -29,7 +29,7 @@ export default class command extends BaseCommand {
             )
         const info = await this.client.DB.getUser(users[0])
         if (
-                ((this.client.config.mods.includes(user) || (info.banned) === 'ban') ||
+                ((this.client.config.mods.includes(users) || (info.banned) === 'ban') ||
                 (!info.banned === 'unban')
             )
             return void reply(`🟥 *@${users[0].split('@')[0]}* is a MOD & can't be banned.`)
