9596a20c4f37c3da0f080b73fe046e34c533e501
一茶 <Issamoha020@gmail.com>
2023-05-19 11:01:41 +0300

    well...

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -17,17 +17,20 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
         super()
         Config()
         this.config = {
-            name: 'KATSUSHIKA',
-            session: '454',
-            prefix: '#',
-            mods: ["254781967541"],
+            name: process.env.BOT_NAME || 'Bot',
+            session: process.env.SESSION || 'SESSION',
+            prefix: process.env.PREFIX || ':',
+            mods: (process.env.MODS || '').split(', ').map((user) => `${user}@s.whatsapp.net`),
             PORT: Number(process.env.PORT || 3000)
         }
         new Server(this)
     }
 
     public start = async (): Promise<client> => {
-        await connect('mongodb+srv://steveinkkz:issamoha@cluster0.l6hudq5.mongodb.net/?retryWrites=true&w=majority')
+        if (!process.env.MONGO_URI) {
+            throw new Error('No MongoDB URI provided')
+        }
+        await connect(process.env.MONGO_URI)
         this.log('Connected to the Database')
         const { useDatabaseAuth } = new AuthenticationFromDatabase(this.config.session)
         const { saveState, state, clearState } = await useDatabaseAuth()
